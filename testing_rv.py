c10eae9a5cbfa71523fa422477adb865dc5f0bfb
Andy Casey <andycasey@gmail.com>
2016-03-24 10:59:28 +0000

    Testing script for until #8 is closed

---
+++ testing_rv.py
@@ -0,0 +1,13 @@
+from smh.session import Session
+
+"""
+1. Create a new session from multi-spec files.
+2. Measure the radial velocity from an un-normalsied order.
+3. Apply that radial velocity measurement to the session.
+"""
+
+a = Session([
+    "/Users/arc/codes/smh/hd44007red_multi.fits",
+    "/Users/arc/codes/smh/hd44007blue_multi.fits",
+    ])
+
