b97d4116e5e2a693235fb76c1e1970d7a751c4ca
Andy Casey <andycasey@gmail.com>
2016-04-10 20:21:13 +0100

    Add fitting function to profile sub-class of the measurement object

--- smh/sm/__init__.py
+++ smh/sm/__init__.py
@@ -10,3 +10,33 @@ from __future__ import (division, print_function, absolute_import,
 from .base import *
 from .profile import *
 from .synthesis import *
+
+
+
+# Inputs common to EW and synthesis:
+# - transitions
+# - parent session
+
+# Properties common to EW and synthesis:
+# - transitions
+# - parent session
+# - metadata
+# - fitting_method (be as part of metadata?)
+# - parameters (named, and values if they are optimized/fixed)
+# - bounds (bounds on parameters)
+# - fixed (whether parameters are fixed or not)
+
+# Methods common to EW and syntheses:
+# - __call__ function to produce spectra given the model parameters
+# - abundance property (internally store an abundance and the params it was calculated on? then only re-calculate from session model if those params
+# have been updated??) --> probably not because that will TRIGGER, but do something clever
+
+
+# EW-only:
+# fit(spectrum, wl_range, *fitting_args)
+# --> return optimised fitting parameters
+
+
+# Synthesis-only:
+# fit(spectrum, wl_range, teff, logg, which_atmospheres, etc, *fitting_args)
+# --> returns optimized fitting parameters including abundances, etc
