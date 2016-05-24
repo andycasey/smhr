f7a2850219ad0e5197c196503fba5eaf74645c4e
Alexander Ji <alexji@mit.edu>
2016-05-23 22:27:02 -0400

    Default ivar is ones (#4)

---
+++ smh/tests/test_rvcor.py
@@ -0,0 +1,20 @@
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+import os
+from smh.session import Session
+import yaml
+
+datadir = os.path.dirname(os.path.abspath(__file__))+'/test_data'
+
+def test_rv():
+    session = Session([datadir+'/spectra/hd122563.fits'])
+    with open(Session._default_settings_path, "rb") as fp:
+        defaults = yaml.load(fp)
+    session.metadata.update(defaults)
+    rv, rv_uncertainty = session.rv_measure()
+    session.rv_correct(rv)
+
+if __name__=="__main__":
+    test_rv()
+
