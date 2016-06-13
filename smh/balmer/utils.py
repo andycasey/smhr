93849105cf6a269db32f4de932e1370dc3c5b333
Andy Casey <andycasey@gmail.com>
2016-06-13 23:56:40 +0100

    Added grunt code to model balmer lines from Barklem grid

---
+++ smh/balmer/utils.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Utility functions. """
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+import os
+import numpy as np
+
+__all__ = ["parse_spectrum", "parse_stellar_parameters"]
+
+
+def parse_stellar_parameters(path):
+    """
+    Parse the stellar parameters from the path name.
+
+    :param path:
+        The path containing computed spectra:
+        (e.g. "alf_synth_t5500g2.90m-2.00x1.5.prf")
+    """
+
+    basename = os.path.basename(path).split("_")[-1]
+    teff, logg, mh = (float(each) for each in \
+        (basename[1:5], basename[6:10], basename[11:16].rstrip("x")))
+    return (teff, logg, mh)
+
+
+def parse_spectrum(path):
+    """
+    Parse the spectrum from the provided path.
+
+    :param path:
+        The path containing computed spectra.
+    """
+
+    with open(path, "r") as fp:
+        contents = fp.read().split("\n")[3:-3]
+
+    return np.array([point.split() for point in contents], dtype=float)
