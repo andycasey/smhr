efd3d8254ec209178149d47c528770c3bc551a82
Andy Casey <andycasey@gmail.com>
2017-09-29 12:48:16 +1000

    initial

---
+++ smh/radiative_transfer/turbospectrum/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" A standardized interface to the MOOG(SILENT) radiative transfer code. """
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+#__all__ = ["abundance_cog", "synthesize", "RTError"]
+
+# See stackoverflow.com/questions/19913653/no-unicode-in-all-for-a-packages-init
+#__all__ = [_.encode("ascii") for _ in __all__]
